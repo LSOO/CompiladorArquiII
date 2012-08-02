03cc5c5a9990feb411d713b40b7ecd48a9a1fa3e
Alejandro Cardona <carealejo@localhost.localdomain>
2012-08-02 17:31:02 -0500

    Get lines from the code

--- P1-VN/VonNeumanABinario/mainwindow.cpp
+++ P1-VN/VonNeumanABinario/mainwindow.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,13 +18,41 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+static vector<string> extractwords(string line){
+
+    vector<string> words;
+    string pal;
+    string s=" ";
+    string c=",";
+    for(int unsigned i=0;i<line.size();i++){
+        cout << line[i] << endl;
+        /*if(line[i].c and line[i]!=c){
+            cout << line[i] << endl;
+        }*/
+    }
+    cout << "PASS" << endl;
+
+}
+
 void MainWindow::on_pushButton_clicked()
 {
-    ofstream fs("nombre.txt");
+    vector<string> v;
 
+    //Get text from user
+    string line;
+    ofstream fs("code.txt");
     fs << ui->textEdit->toPlainText().toStdString() << endl;
-
-
     fs.close();
+
+    //Extract text from code.txt
+    fstream tex;
+    tex.open("code.txt");
+    while (! tex.eof() ){
+      getline (tex,line);
+      cout << "PASS" << endl;
+      //v = extractwords(line);
+    }
+
+    tex.close();
 
 }
